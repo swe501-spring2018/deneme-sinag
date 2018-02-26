4dbbe7b8b7d2d722913bbbf30651008d5d148eb2
Yunus Sina Gülşen <ygulsen@datamarket.com.tr>
2018-02-26 21:08:55 +0300

    write my name example added

---
+++ myname.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : myname.cpp
+// Author      : yunus sina gulsen
+// Version     :
+// Copyright   : free as in beer
+// Description : write my name in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "yunus sina gulsen" << endl; // prints my name
+	return 0;
+}
